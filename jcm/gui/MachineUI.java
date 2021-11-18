12b2437ab837a80c6487a10197c69db7a2f42152
art1sec <schmitz@msitc.de>
2021-11-18 11:11:06 +0100

    implemented load and save

--- jcm/gui/MachineUI.java
+++ jcm/gui/MachineUI.java
@@ -10,6 +10,12 @@ import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -30,145 +36,181 @@ class MyButton extends JButton {
 
 public class MachineUI {
 
-  static final boolean shouldFill = true;
-  static final boolean shouldWeightX = true;
+    static final boolean shouldFill = true;
+    static final boolean shouldWeightX = true;
 
-  public int pointer = 0;
-  public JFrame frame;
-  private JButton button;
+    public int pointer = 0;
+    public JFrame frame;
+    private JButton button;
 
-  public JTextField[] prog;
-  public JTextField[] reg;
-  public JLabel[] point;
+    public JTextField[] prog;
+    public JTextField[] reg;
+    public JLabel[] point;
 
-  private void fillGrid(Container pane) {
-    pane.setLayout(new GridBagLayout());
-    GridBagConstraints c = new GridBagConstraints();
-    if (shouldFill) {
-      // natural height; maximum width
-      c.fill = GridBagConstraints.HORIZONTAL;
-    }
+    java.net.URL url = getClass().getResource("../res/icons8-left-arrow-48.png");
+    ImageIcon ico = new ImageIcon(url, "<<---<");
 
-    prog = new JTextField[20];
-    point = new JLabel[20];
-    for (int i = 1; i <= 20; i++) {
-      c.gridx = 0;
-      c.gridy = i;
-      c.ipady = 10;
-      button = new JButton(Integer.toString(i));
-      pane.add(button, c);
-      c.gridx = 1;
-      c.gridy = i;
-      prog[i - 1] = new JTextField(5);
-      pane.add(prog[i - 1], c);
-      c.gridx = 2;
-      c.gridy = i;
-      point[i - 1] = new JLabel("     ");
-      pane.add(point[i - 1], c);
-    }
-    reg = new JTextField[6];
-    for (int i = 1; i <= 6; i++) {
-      c.gridx = 3;
-      c.gridy = i;
-      button = new JButton(Character.toString((char) (64 + i)));
-      pane.add(button, c);
-      c.gridx = 4;
-      c.gridy = i;
-      reg[i - 1] = new JTextField(5);
-      pane.add(reg[i - 1], c);
-    }
-    setPointer(0,0);
-  }
-
-  private void fillToolBar(Container pane) {
-    MyButton load = new MyButton("Load");
-    MyButton save = new MyButton("Save");
-    MyButton reset = new MyButton("Reset");
-    MyButton run = new MyButton("Run");
-    pane.add(load);
-    pane.add(save);
-    pane.add(reset);
-    pane.add(run);
-    save.addActionListener(e -> {
-        //Parser p = new Parser(this);
-        File myfile = new File("cmprogram.cmp");
-        JFileChooser saveDialog = new JFileChooser();
-        int returnValue = saveDialog.showOpenDialog(frame);
-        if (returnValue == JFileChooser.APPROVE_OPTION) {
-            myfile = saveDialog.getSelectedFile();
-            System.out.println(myfile.getAbsoluteFile());
-            System.out.println(myfile.getName());
-        } else {
-            System.out.println("canceled");
+    private void fillGrid(Container pane) {
+        pane.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        if (shouldFill) {
+            // natural height; maximum width
+            c.fill = GridBagConstraints.HORIZONTAL;
         }
-    });
-    reset.addActionListener(e -> {
-        setPointer(pointer, 0);
-    });
-    run.addActionListener(new RunProgram(this));
-  }
 
-  public void createAndShowGUI() {
-    // if (System.getProperty("os.name").startsWith("Linux")) {
-    try {
-      UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-    } catch (Exception e) {
-      e.printStackTrace();
+        prog = new JTextField[20];
+        point = new JLabel[20];
+        for (int i = 1; i <= 20; i++) {
+            c.gridx = 0;
+            c.gridy = i;
+            c.ipady = 10;
+            button = new JButton(Integer.toString(i));
+            pane.add(button, c);
+            c.gridx = 1;
+            c.gridy = i;
+            prog[i - 1] = new JTextField(5);
+            pane.add(prog[i - 1], c);
+            c.gridx = 2;
+            c.gridy = i;
+            point[i - 1] = new JLabel("     ");
+            pane.add(point[i - 1], c);
+        }
+        reg = new JTextField[6];
+        for (int i = 1; i <= 6; i++) {
+            c.gridx = 3;
+            c.gridy = i;
+            button = new JButton(Character.toString((char) (64 + i)));
+            pane.add(button, c);
+            c.gridx = 4;
+            c.gridy = i;
+            reg[i - 1] = new JTextField(5);
+            pane.add(reg[i - 1], c);
+        }
+        setPointer(0, 0);
     }
-    // }
 
-    frame = new JFrame("Simple Counter Machine");
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-    JLabel head = new JLabel("Simple Counter Machine");
-    head.setFont(new Font("Sans", Font.PLAIN, 20));
-    head.setHorizontalAlignment(JLabel.CENTER);
-    head.setBorder(BorderFactory.createEmptyBorder(8, 0, 16, 0));
-    frame.add(head, BorderLayout.NORTH);
-
-    JPanel panel = new JPanel();
-    fillGrid(panel);
-    frame.add(panel);
-
-    JPanel toolbar = new JPanel();
-    fillToolBar(toolbar);
-    toolbar.setFont(new Font("Sans", Font.PLAIN, 16));
-    frame.add(toolbar, BorderLayout.SOUTH);
-
-    frame.pack();
-    frame.setVisible(true);
-  }
-  
-  public void setPointer(int old, int next) {
-      point[pointer].setIcon(null);
-//      ClassLoader cl = this.getClass().getClassLoader();
-//      URL url = cl.getResource("/de/msitc/cmachine/res/icons8-left-arrow-48.png");
-//      URL url = getClass().getResource("/de/msitc/cmachine/res/icons8-left-arrow-48.png");
-//      Image img = new ImageIcon(url).getImage();
-//      ImageIcon ico = new ImageIcon(img);
-//      point[next].setIcon(ico);
-      //point[next].setIcon(new ImageIcon("images/icons8-left-arrow-48.png"));
-      java.net.URL url = getClass().getResource("../res/icons8-left-arrow-48.png");
-      ImageIcon ico = new ImageIcon(url, "<----");
-      point[next].setIcon(ico);
-      System.out.println(url);
-      pointer = next;
-  }
-
-  class RunProgram implements ActionListener {
-      private MachineUI machine;
-      RunProgram(MachineUI m) {
-          this.machine = m;
-      }
-    @Override
-    public void actionPerformed(ActionEvent e) {
-      Parser p = new Parser(machine);
-      Thread m = new Machine(machine, p.getProgram(), p.getRegister(), machine.pointer);
-      m.start();
+    private void fillToolBar(Container pane) {
+        MyButton load = new MyButton("Load");
+        MyButton save = new MyButton("Save");
+        MyButton reset = new MyButton("Reset");
+        MyButton run = new MyButton("Run");
+        pane.add(load);
+        pane.add(save);
+        pane.add(reset);
+        pane.add(run);
+        load.addActionListener(new LoadProgram());
+        save.addActionListener(new SaveProgram(this));
+        reset.addActionListener(e -> {
+            setPointer(pointer, 0);
+        });
+        run.addActionListener(new RunProgram(this));
     }
-  }
-  
-//  class SaveProgram implements ActionListener {
-      
-//  }
+
+    public void createAndShowGUI() {
+        // if (System.getProperty("os.name").startsWith("Linux")) {
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // }
+
+        frame = new JFrame("Simple Counter Machine");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel head = new JLabel("Simple Counter Machine");
+        head.setFont(new Font("Sans", Font.PLAIN, 20));
+        head.setHorizontalAlignment(JLabel.CENTER);
+        head.setBorder(BorderFactory.createEmptyBorder(8, 0, 16, 0));
+        frame.add(head, BorderLayout.NORTH);
+
+        JPanel panel = new JPanel();
+        fillGrid(panel);
+        frame.add(panel);
+
+        JPanel toolbar = new JPanel();
+        fillToolBar(toolbar);
+        toolbar.setFont(new Font("Sans", Font.PLAIN, 16));
+        frame.add(toolbar, BorderLayout.SOUTH);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public void setPointer(int old, int next) {
+        point[pointer].setIcon(null);
+        point[next].setIcon(ico);
+        // System.out.println(url);
+        pointer = next;
+    }
+
+    class RunProgram implements ActionListener {
+        private MachineUI machine;
+
+        RunProgram(MachineUI m) {
+            this.machine = m;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            Parser p = new Parser(machine);
+            Thread m = new Machine(machine, p.getProgram(), p.getRegister(), machine.pointer);
+            m.start();
+        }
+    }
+
+    class SaveProgram implements ActionListener {
+        private MachineUI machine;
+
+        SaveProgram(MachineUI m) {
+            this.machine = m;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            File myfile = new File("cmprogram.cmp");
+            JFileChooser saveDialog = new JFileChooser();
+            int returnValue = saveDialog.showOpenDialog(frame);
+            if (returnValue == JFileChooser.APPROVE_OPTION) {
+                myfile = saveDialog.getSelectedFile();
+                System.out.println(myfile.getAbsoluteFile());
+                try {
+                    FileOutputStream fos = new FileOutputStream(myfile);
+                    ObjectOutputStream oos = new ObjectOutputStream(fos);
+                    Parser p = new Parser(machine);
+                    oos.writeObject(p.getProgram());
+                    oos.close();
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                }
+            } else {
+                System.out.println("canceled");
+            }
+        }
+    }
+
+    class LoadProgram implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JFileChooser loadDialog = new JFileChooser();
+            int returnValue = loadDialog.showOpenDialog(frame);
+            if (returnValue == JFileChooser.APPROVE_OPTION) {
+                File myfile = loadDialog.getSelectedFile();
+                String[][] program = new String[20][2];
+                try {
+                    FileInputStream fis = new FileInputStream(myfile);
+                    ObjectInputStream ois = new ObjectInputStream(fis);
+                    program = (String[][]) ois.readObject();
+                    ois.close();
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                } catch (ClassNotFoundException cnfe) {
+                    cnfe.printStackTrace();
+                }
+                for (int i = 0; i < 20; i++) {
+                    prog[i].setText(program[i][0]+" "+program[i][1]);
+                }
+            }
+        }
+    }
 }
--- jcm/machine/Parser.java
+++ jcm/machine/Parser.java
@@ -4,50 +4,51 @@ import jcm.gui.*;
 
 public class Parser {
 
-  private MachineUI machine;
-  private int[] register;
-  private String[][] program;
+    private MachineUI machine;
+    private int[] register;
+    private String[][] program;
 
-  public Parser(MachineUI m) {
-    this.machine = m;
-    register = new int[6];
-    program = new String[20][2];
-    parseProgram();
-  }
+    public Parser(MachineUI m) {
+        this.machine = m;
+        register = new int[6];
+        program = new String[20][2];
+        parseProgram();
+    }
 
-  public void parseProgram() {
-    for (int i = 0; i < 20; i++) {
-        String field = machine.prog[i].getText();
-        if (field.equals("Stop")) {
-            program[i][0] = "Stop";
-            program[i][1] = "";
-        } else {
-            try {
-                String f[] = field.split(" ", 2);
-                program[i][0] = f[0];
-                program[i][1] = f[1];
-            } catch (Exception e) {
-                program[i][0] = "";
+    public void parseProgram() {
+        for (int i = 0; i < 20; i++) {
+            String field = machine.prog[i].getText();
+            if (field.equals("Stop")) {
+                program[i][0] = "Stop";
                 program[i][1] = "";
+            } else {
+                try {
+                    String f[] = field.split(" ", 2);
+                    program[i][0] = f[0];
+                    program[i][1] = f[1];
+                } catch (Exception e) {
+                    program[i][0] = "";
+                    program[i][1] = "";
+                }
+            }
+        }
+        for (int i = 0; i < 6; i++) {
+            if (machine.reg[i].getText().length() > 0) {
+                register[i] = Integer.parseInt(machine.reg[i].getText());
+            } else {
+                register[i] = 0;
             }
         }
     }
-    for (int i = 0; i < 6; i++) {
-        if (machine.reg[i].getText().length() > 0) {
-            register[i] = Integer.parseInt(machine.reg[i].getText());
-        } else {
-            register[i] = 0;
-        }
-    }
-  }
 
-  public String[][] getProgram() {
-//      for (String[] s: program) {
-//          System.out.println(s[0]+" "+s[1]);
-//      }
-      return program;
-  }
-  public int[] getRegister() {
-      return register;
-  }
+    public String[][] getProgram() {
+        // for (String[] s: program) {
+        // System.out.println(s[0]+" "+s[1]);
+        // }
+        return program;
+    }
+
+    public int[] getRegister() {
+        return register;
+    }
 }
